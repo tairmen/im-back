912d7a365da0515263497a1a7c63eefa05ad0661
Tair <abduraimovtair@gmail.com>
2022-09-12 16:11:35 +0300

    make current request

--- routes/main/index.js
+++ routes/main/index.js
@@ -1,8 +1,10 @@
 module.exports = function (app, models, auth) {
-    app.get("/welcome", auth, (req, res) => {
+    app.get("/current", auth, async (req, res) => {
+        const user = await models.User.findOne({ where: { email: req.user.email } });
         res.status(200).send({
             success: true,
-            message: "Welcome 🙌 "
+            message: "Welcome 🙌 ",
+            data: user
         });
     });
 }
